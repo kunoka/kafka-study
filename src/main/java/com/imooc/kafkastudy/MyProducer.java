86f34a201a88e8cb2dfcf80f2bbe414e5dc7e678
Harry <hengwang@cn.ibm.com>
2019-12-17 10:55:58 +0800

    12-4 使用原生API发送消息（2）

--- src/main/java/com/imooc/kafkastudy/MyProducer.java
+++ src/main/java/com/imooc/kafkastudy/MyProducer.java
@@ -1,7 +1,9 @@
 package com.imooc.kafkastudy;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
 
@@ -16,7 +18,7 @@ public class MyProducer {
     producer = new KafkaProducer<String, String>(properties);
   }
 
-  // 发送消息
+  // 发送消息 1
   private static void sendMessageForgetResult() {
     ProducerRecord<String, String> record = new ProducerRecord<String, String>(
       "imooc-kafka-study", "name", "ForgetResult"
@@ -25,7 +27,45 @@ public class MyProducer {
     producer.close();
   }
 
-  public static void main(String[] args) {
-    sendMessageForgetResult();
+  // 发送消息 2 - 同步发送
+  private static void sendMessageSync() throws Exception {
+    ProducerRecord<String, String> record = new ProducerRecord<String, String>(
+      "imooc-kafka-study", "name", "sync"
+    );
+    RecordMetadata result = producer.send(record).get();
+    System.out.println(result.topic());
+    System.out.println(result.partition());
+    System.out.println(result.offset());
+
+    producer.close();
+  }
+
+  // 发送消息 3 - 异步发送
+  private static void sendMessageCallback() {
+    ProducerRecord<String, String> record = new ProducerRecord<String, String>(
+      "imooc-kafka-study", "name", "callback"
+    );
+    producer.send(record, new MyProducerCallback());
+    producer.close();
+  }
+
+  private static class MyProducerCallback implements Callback {
+    @Override
+    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+      if (e != null) {
+        e.printStackTrace();
+        return;
+      }
+      System.out.println(recordMetadata.topic());
+      System.out.println(recordMetadata.partition());
+      System.out.println(recordMetadata.offset());
+      System.out.println("Coming in MyProducerCallback");
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+//    sendMessageForgetResult();
+//    sendMessageSync();
+    sendMessageCallback();
   }
 }
